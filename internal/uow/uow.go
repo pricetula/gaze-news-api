d66ad82fc58ca011793708320241506857ef1ac1
Price Tula <pricetula@gmail.com>
2025-05-22 22:19:08 +0300

    add unit of work util to create transactions, commit or rollback incase of error in commited transaction actions

---
+++ internal/uow/uow.go
@@ -0,0 +1,50 @@
+package uow
+
+import (
+	"context"
+
+	"github.com/jmoiron/sqlx"
+	"github.com/pricetula/gaze-news-api/internal/domain"
+	"github.com/pricetula/gaze-news-api/internal/repository"
+)
+
+type Repositories struct {
+	ArticleRepository domain.ArticleRepository
+}
+
+type UnitOfWork interface {
+	Do(ctx context.Context, fn func(r *Repositories) error) error
+}
+
+type unitOfWork struct {
+	db *sqlx.DB
+}
+
+func New(db *sqlx.DB) UnitOfWork {
+	return &unitOfWork{db}
+}
+
+func (u *unitOfWork) Do(ctx context.Context, fn func(r *Repositories) error) error {
+	// Start a new transaction
+	tx, err := u.db.BeginTxx(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	// Ensure the transaction is rolled back if the function returns an error
+	defer tx.Rollback()
+
+	// Create a new instance of Repositories with the transaction
+	repos := &Repositories{
+		ArticleRepository: repository.NewArticlesRepository(tx),
+	}
+
+	// Execute the function with the repositories. If it returns an error, rollback the transaction
+	if err := fn(repos); err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	// Commit the transaction if everything went well
+	return tx.Commit()
+}
